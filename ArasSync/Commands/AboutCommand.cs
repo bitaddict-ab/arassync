d33b789b1756546020dc7d0992e1febe74858b06
Marcus Sonestedt <marcus.lindblom.sonestedt@bitaddict.se>
2019-04-12 14:23:06 +0200

    ArasSync: Add 'about' command

---
+++ ArasSync/Commands/AboutCommand.cs
@@ -0,0 +1,26 @@
+﻿// MIT License, see COPYING.TXT
+using System;
+using BitAddict.Aras.ArasSync.Properties;
+using JetBrains.Annotations;
+using ManyConsole;
+
+namespace BitAddict.Aras.ArasSync.Commands
+{
+    /// <summary>
+    /// Shows contents of COPYING.TXT
+    /// </summary>
+    [UsedImplicitly]
+    public class AboutCommand : ConsoleCommand
+    {
+        public AboutCommand()
+        {
+            IsCommand("About", "Shows full license/copyright notice.");
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            Console.WriteLine(Resources.COPYING);
+            return 0;
+        }
+    }
+}
