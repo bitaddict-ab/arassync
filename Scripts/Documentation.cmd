53fb50f6a4154305d9447175c65df2a0b5204163
Marcus Sonestedt <marcus.sonestedt@bitaddict.se>
2019-04-25 09:26:34 +0200

    Scripts: Restore nuget package for Docs project explicitly

--- Scripts/Documentation.cmd
+++ Scripts/Documentation.cmd
@@ -9,11 +9,15 @@ cd /D %~dp0..
 call %~dp0\_RestorePackages.cmd
 if ERRORLEVEL 1 exit /b %ERRORLEVEL%
 
+call %~dp0\_nuget.cmd restore .\Docs\Docs.csproj -SolutionDir %CD%
+if ERRORLEVEL 1 exit /b %ERRORLEVEL%
+
+
 echo.
 echo === Generating HTML documentation into Documentation\_site ===
 echo.
 
-call %~dp0\_msbuild.cmd .\Docs\Docs.csproj /verbosity:minimal /filelogger
+call %~dp0\_msbuild.cmd .\Docs\Docs.csproj /t:Build /verbosity:minimal /filelogger
 echo.
 
 IF ERRORLEVEL 1 (
