d4f86a33817e29ec1a19c5d80759ad955cc4e535
Daniel Jonsson <daniel.jonsson@bitaddict.se>
2019-10-22 13:27:46 +0200

    Add missing using statement

--- BitAddict.Aras/ArasExtensions.cs
+++ BitAddict.Aras/ArasExtensions.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Xml;
 using Aras.IOM;
